e6bc314c5af9ba96b72feb4698b80d4b0372edbf
Liu Zhiwen <37220222203708@stu.xmu.edu.cn>
2024-11-26 22:58:59 +0800

    Delete 8.SpringTest directory

--- 8.SpringTest/TestTomcat/runjava.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash --
-
-java -Xms1g -Xmx1g -Xss512k \
-     -Dcom.sun.management.jmxremote.port=3214 \
-     -Dcom.sun.management.jmxremote.ssl=false \
-     -Dcom.sun.management.jmxremote.authenticate=false \
-     -jar $1
